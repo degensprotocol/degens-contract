fe25a8c6930b181b97b1ec5f27239492012d953f
Degens Dev Team <support@degens.com>
2019-08-20 21:38:10 -0400

    extend the timing intervals used when simulating order expirations
    - This was failing on some very slow environments (ie coverage tests inside a docker)

--- t/matchFailures.js
+++ t/matchFailures.js
@@ -10,10 +10,10 @@ testlib.doTests([
     { action: 'deposit', from: 'B', amount: 10000, },
 
     { action: 'order', from: 'A', amount: 1000, dir: 'buy', price: 60, orderId: 1, expiryOffset: 1000, },
-    { action: 'order', from: 'B', amount: 500, dir: 'sell', price: 40, orderId: 2, expiryOffset: 30, },
-    { action: 'order', from: 'B', amount: 500, dir: 'sell', price: 30, orderId: 3, expiryOffset: 60, },
+    { action: 'order', from: 'B', amount: 500, dir: 'sell', price: 40, orderId: 2, expiryOffset: 300, },
+    { action: 'order', from: 'B', amount: 500, dir: 'sell', price: 30, orderId: 3, expiryOffset: 600, },
 
-    { action: 'increase-time', amount: 40, },
+    { action: 'increase-time', amount: 400, },
 
     { action: 'matchOrders', from: 'C', leftOrderId: 1, rightOrderIds: [2], expectLogs: [["LogTradeError", "ORDER_EXPIRED"]], },
     { action: 'assert', balances: { A: 10000, B: 10000 }, positions: { A: 0, B: 0, } },
@@ -37,10 +37,10 @@ testlib.doTests([
     { action: 'deposit', from: 'B', amount: 10000, },
 
     { action: 'order', from: 'A', amount: 1000, dir: 'buy', price: 60, orderId: 1, expiryOffset: 1000, },
-    { action: 'order', from: 'B', amount: 500, dir: 'sell', price: 40, orderId: 2, expiryOffset: 30, },
-    { action: 'order', from: 'B', amount: 500, dir: 'sell', price: 30, orderId: 3, expiryOffset: 60, },
+    { action: 'order', from: 'B', amount: 500, dir: 'sell', price: 40, orderId: 2, expiryOffset: 300, },
+    { action: 'order', from: 'B', amount: 500, dir: 'sell', price: 30, orderId: 3, expiryOffset: 600, },
 
-    { action: 'increase-time', amount: 40, },
+    { action: 'increase-time', amount: 400, },
 
     { action: 'matchOrders', from: 'C', leftOrderId: 1, rightOrderIds: [3, 2], expectLogs: [["LogTrade"], ["LogTrade"], ["LogTradeError", "ORDER_EXPIRED"]], },
     { action: 'assert', balances: { A: 9573, B: 9501, C: 214, }, positions: { A: 712, B: -712, C: 0, } },
@@ -57,11 +57,11 @@ testlib.doTests([
     { action: 'deposit', from: 'A', amount: 10000, },
     { action: 'deposit', from: 'B', amount: 10000, },
 
-    { action: 'order', from: 'A', amount: 1000, dir: 'buy', price: 60, orderId: 1, expiryOffset: 30, },
-    { action: 'order', from: 'B', amount: 500, dir: 'sell', price: 40, orderId: 2, expiryOffset: 60, },
-    { action: 'order', from: 'B', amount: 500, dir: 'sell', price: 30, orderId: 3, expiryOffset: 60, },
+    { action: 'order', from: 'A', amount: 1000, dir: 'buy', price: 60, orderId: 1, expiryOffset: 300, },
+    { action: 'order', from: 'B', amount: 500, dir: 'sell', price: 40, orderId: 2, expiryOffset: 600, },
+    { action: 'order', from: 'B', amount: 500, dir: 'sell', price: 30, orderId: 3, expiryOffset: 600, },
 
-    { action: 'increase-time', amount: 40, },
+    { action: 'increase-time', amount: 400, },
 
     { action: 'matchOrders', from: 'C', leftOrderId: 1, rightOrderIds: [2, 3], expectLogs: [['LogTradeError', 'TAKER_NO_BALANCE'], ['LogTradeError', 'TAKER_NO_BALANCE']], },
     { action: 'assert', balances: { A: 10000, B: 10000 }, positions: { A: 0, B: 0, } },
--- t/orderExpiry.js
+++ t/orderExpiry.js
@@ -9,9 +9,9 @@ testlib.doTests([
     { action: 'deposit', from: 'A', amount: 10000, },
     { action: 'deposit', from: 'B', amount: 10000, },
 
-    { action: 'order', from: 'A', amount: 1000, dir: 'buy', price: 40, orderId: 1, expiryOffset: 30, },
+    { action: 'order', from: 'A', amount: 1000, dir: 'buy', price: 40, orderId: 1, expiryOffset: 300, },
 
-    { action: 'increase-time', amount: 40, },
+    { action: 'increase-time', amount: 400, },
 
     { action: 'trade', from: 'B', orderIds: [1], amount: 500, expectLogs: [['LogTradeError', 'ORDER_EXPIRED']], },
 
@@ -25,13 +25,13 @@ testlib.doTests([
     { action: 'deposit', from: 'A', amount: 10000, },
     { action: 'deposit', from: 'B', amount: 10000, },
 
-    { action: 'order', from: 'A', amount: 1000, dir: 'buy', price: 40, orderId: 1, expiryOffset: 30, },
+    { action: 'order', from: 'A', amount: 1000, dir: 'buy', price: 40, orderId: 1, expiryOffset: 300, },
 
     { action: 'trade', from: 'B', orderIds: [1], amount: 500, expectLogs: [['LogTrade']], },
 
     { action: 'assert', balances: { A: 9667, B: 9501, }, positions: { A: 832, B: -832, } },
 
-    { action: 'increase-time', amount: 40, },
+    { action: 'increase-time', amount: 400, },
 
     { action: 'trade', from: 'B', orderIds: [1], amount: 500, expectLogs: [['LogTradeError', 'ORDER_EXPIRED']], },
 
